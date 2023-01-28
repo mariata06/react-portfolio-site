3de2e6dbc35009afa21476d84158134ef77ca0a7
mariata06 <slinkywizard@mail.ru>
2023-01-28 15:37:09 +0300

    Выносит стили компонентов в styles.js, добавляет шрифты, стилизует блок FaqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from 'react';
+//Styles 
+import { About } from '../styles';
+import styled from 'styled-components';
 
 const FaqSection = () => {
     return(
-        <div className='faq'>
+        <Faq>
             <div className="description">
                 
                 <h2>Any Questions <span>FAQ</span></h2>
@@ -13,6 +16,7 @@ const FaqSection = () => {
                         <p>lorem ipsum dolor sit amet.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec purus commodo, pharetra tellus eu, aliquet neque.</p>
                     </div>
+                    <div className='faq-line'></div> 
                 </div>
                 <div className='question'>
                     <h4>Daily Schedule</h4>
@@ -20,25 +24,54 @@ const FaqSection = () => {
                         <p>lorem ipsum dolor sit amet.</p>
                         <p>Nunc elementum accumsan dolor ut facilisis. Fusce rutrum nulla id augue consequat placerat.</p>
                     </div>
+                    <div className='faq-line'></div> 
                 </div>
                 <div className='question'>
                     <h4>Different Payment Methods</h4>
                     <div className='answer'>
-                    <p>lorem ipsum dolor sit amet.</p>
-                    <p>Nullam aliquet auctor purus, eget vestibulum urna vulputate id. Aliquam ut tempus eros.</p>
+                        <p>lorem ipsum dolor sit amet.</p>
+                        <p>Nullam aliquet auctor purus, eget vestibulum urna vulputate id. Aliquam ut tempus eros.</p>
                     </div>
+                    <div className='faq-line'></div> 
                 </div>
                 <div className='question'>
                     <h4>What products do you offer?</h4>
                     <div className='answer'>
-                    <p>lorem ipsum dolor sit amet.</p>
-                    <p>Vestibulum rhoncus velit molestie placerat faucibus. Donec blandit ac massa at efficitur. Nulla ut tempus purus. Fusce in molestie risus.</p>
+                        <p>lorem ipsum dolor sit amet.</p>
+                        <p>Vestibulum rhoncus velit molestie placerat faucibus. Donec blandit ac massa at efficitur. Nulla ut tempus purus. Fusce in molestie risus.</p>
                     </div>
+                    <div className='faq-line'></div> 
                 </div>
             </div>
-        </div>
+        </Faq>
     )
 }
 
+const Faq = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer; 
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
